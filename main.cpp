4eb23822854e42f0d27afdbf13ebe98eb51f214d
EgorPristanskiy <pristanskiy27@gmail.com>
2025-02-19 21:32:30 +0000

    Add base sources for kv storage

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "baseStorage.h"
 
 int main() {
-    std::cout << "AAA" << std::endl;
+    storage::BaseStorage storage;
+    storage.put("123", "567");
+    std::cout << "123 " << ": " << storage.get("123") << std::endl;
     return 0;
 }
---
+++ src/baseStorage.cpp
@@ -0,0 +1,26 @@
+#include <mutex>
+#include "baseStorage.h"
+
+namespace storage {
+bool BaseStorage::put(const std::string& key, const std::string& value) {
+    std::lock_guard<std::mutex> lock_guard(mtx_);
+    storage_[key] = value;
+    return true;
+}
+
+bool BaseStorage::remove(const std::string& key) {
+    std::lock_guard<std::mutex> lock_guard(mtx_);
+    if (storage_.find(key) != storage_.end()) {
+        storage_.erase(key);
+        return true;
+    }
+    return false;
+}
+std::string BaseStorage::get(const std::string& key) {
+    std::lock_guard<std::mutex> lock_guard(mtx_);
+    if (storage_.find(key) != storage_.end()) {
+        return storage_[key];
+    }
+    return "";
+}
+}  // namespace storage
